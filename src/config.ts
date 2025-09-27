2d59b1ac4afcf082ebb114bfa58df46fd44eed19
jaysojitra13 <jaysojitra13@gmail.com>
2025-09-27 08:36:47 +0530

    feat: add config file

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const AMOY_RPC_URL = "https://rpc-amoy.polygon.technology";
+export const OG_RPC_URL = "";
+export const HEDERA_RPC_URL = "https://testnet.hashio.io/api";
+export const IDENTITY_REGISTRY_AMOY = 0xf663447e650a3bcdc041a4dd00c2cd88a1b19bb6;
+// export const DID_VALIDATOR_AMOY=0xF663447E650A3bcdc041A4dD00c2cD88a1B19bB6
+export const IDENTITY_REGISTRY_HEDERA = 0x662d9dba853d407f58285b1ad494909db630cd42;
+// export const DID_VALIDATOR_HEDERA=0x2846653CCc036Db6F1dB305b2665c1d11F9d9381
